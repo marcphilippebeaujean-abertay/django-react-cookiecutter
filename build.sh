97666b4e9268c165f566074526a4c1badaa4b90c
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-03-09 21:08:19 +0100

    removed redundant bash script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+yarn install
+yarn build
+python "./util_scripts/build_frontend/move_files.py"
+
 pip install -r requirements.txt
-./util_scripts/build_frontend/build_frontend.sh
 python manage.py test
 python manage.py collectstatic --no-input
 python manage.py migrate
--- util_scripts/build_frontend/build_frontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-yarn install
-yarn build
-python "${DIR}/move_files.py"
