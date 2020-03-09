fae1bfc9bd2e75c53ff472621be292d511c071aa
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-03-09 08:06:14 +0100

    prevented package json from being ignored

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
-yarn install
 ./util_scripts/build_frontend/build_frontend.sh
 python manage.py test
 python manage.py collectstatic --no-input
--- util_scripts/build_frontend/build_frontend.sh
+++ util_scripts/build_frontend/build_frontend.sh
@@ -2,6 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+yarn install
 yarn build
 python "${DIR}/move_files.py"
 python "${DIR}/sanitise_index_html.py"
