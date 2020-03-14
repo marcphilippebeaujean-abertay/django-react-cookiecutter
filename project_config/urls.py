1b3bd41169a6df5ff7596c9854a263452b21c674
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-03-14 19:33:27 +0100

    fixed api and password reset not working

--- project_config/urls.py
+++ project_config/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
+from django.conf.urls import url
 
 urlpatterns = [
-    path('api/api-auth/', include('rest_framework.urls')),
+    path('api/', include('rest_framework.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
     path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
     path('api/real-admin/', admin.site.urls),
 
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 
+    url(r'^', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
 ]
