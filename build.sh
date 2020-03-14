ed23c30e85adf6f14145ecfc6b25050dbf2450de
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-03-14 14:46:19 +0100

    moved frontend to its own folder, updated the build script to hopefully work this time

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
-
-yarn install
-yarn build
-python "./util_scripts/build_frontend/move_files.py"
-
-pip install -r requirements.txt
-python manage.py test
-python manage.py collectstatic --no-input
-python manage.py migrate
+#!/usr/bin/env bash
+if ! ./util_scripts/build_frontend/build_frontend.sh; then
+  echo "Frontend build failed."
+  exit 1
+fi
+pip3 install -r requirements.txt
+if ! yes yes | python3 manage.py test; then
+  echo "Tests failed."
+  exit 1
+fi
+python3 manage.py collectstatic --no-input
+if ! python3 manage.py migrate; then
+  echo "Migration failed."
+  exit 1
+fi
--- util_scripts/build_frontend/move_files.py
+++ util_scripts/build_frontend/move_files.py
@@ -2,9 +2,10 @@ import os.path
 import shutil
 
 
-backend_static_dir = os.path.join(os.getcwd(), "static")
-backend_templates_dir = os.path.join(os.getcwd(), "templates")
-frontend_build_dir = os.path.join(os.getcwd(), "build")
+backend_dir = os.path.join(os.getcwd(), '..')
+backend_static_dir = os.path.join(backend_dir, "static")
+backend_templates_dir = os.path.join(backend_dir, "templates")
+frontend_build_dir = os.path.join(backend_dir, "frontend", "build")
 
 backend_index_file = (os.path.join(backend_templates_dir, "index.html"))
 if os.path.isfile(backend_index_file):
@@ -12,8 +13,8 @@ if os.path.isfile(backend_index_file):
 frontend_index_file = (os.path.join(frontend_build_dir, "index.html"))
 if os.path.isfile(frontend_index_file):
     shutil.move(frontend_index_file, backend_templates_dir)
-if os.path.isdir(os.path.join(frontend_build_dir)):
-    backend_static_folder = backend_static_dir
+if os.path.isdir(frontend_build_dir):
+    backend_static_folder = os.path.join(backend_dir, "static")
     shutil.rmtree(backend_static_folder, ignore_errors=True)
     os.mkdir(backend_static_folder)
     frontend_build_static_dir = os.path.join(frontend_build_dir, "static")
@@ -22,4 +23,3 @@ if os.path.isdir(os.path.join(frontend_build_dir)):
     shutil.rmtree(frontend_build_static_dir)
     for f in os.listdir(frontend_build_dir):
         shutil.move(os.path.join(frontend_build_dir, f), backend_static_dir)
-
